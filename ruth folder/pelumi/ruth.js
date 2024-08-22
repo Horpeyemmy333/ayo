fa541efb0036832a882cf02d0d442e5a560a6c69
Horpeyemmy333 <yemmyte2022@gmail.com>
2024-08-22 11:55:48 +0100

    Add files via upload

---
+++ ruth folder/pelumi/ruth.js
@@ -0,0 +1,36 @@
+console.log (typeof 35);
+console.log(20+50);
+console.log(typeof Name);
+console.log(typeof"name");
+console.log("20"+"50");
+console.log("big"+"man");
+console.log("big "+"man");
+console.log("big", "man");
+let a=10 ,b=25
+console.log(a+b);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(b%a);
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+console.log(a=b);
+console.log(a>=b);
+console.log(a++);
+console.log(b--);
+console.log(a+1);
+
+let name="Akinlade daviana";
+const greeting="Hello";
+console.log(greeting+name);
+
+
+let userName=string(prompt("eewade"));
+console.log("Hello" + userName)
+
+
+
+
+
